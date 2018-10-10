1b2fbf48a4e7c2fe5b3e11d58faa3734b3041d37
sama12386 <43897963+sama12386@users.noreply.github.com>
2018-10-09 22:52:08 -0400

    Update cuelist.js

--- assets/js/cuelist.js
+++ assets/js/cuelist.js
@@ -1,33 +1,27 @@
 var data = [
   {
-    "title": "If We Were Birds",
-    "subtitle": "Designed by me",
-    "path":"assets/files/Process/Molitoriss_Aida_Photometric_Analysis.pdf",
-    "description": "This plot was absolutely designed by me"
+    "title": "Stumpy Legs Too Short",
+    "subtitle": "Helen Wayne Rauh Studio Theater, 2018",
+    "path":"assets/files/Process/Molitoriss_Stumpy_Lighting_Cue_List_V5.pdf",
+    "description": ""
   },
   {
-    "title": "Aida",
-    "subtitle": "Designed by me",
-    "path":"assets/files/Process/Molitoriss_Streetcar_Binder.pdf",
-    "description": "This plot was absolutely designed by me"
+    "title": "See What I Wanna See",
+    "subtitle": "Helen Wayne Rauh Studio Theater, 2017",
+    "path":"assets/files/Process/Molitoriss_SWIWS_Lighting_Cue_List_V5.pdf",
+    "description": ""
   },
   {
-    "title": "Lunar Gala",
-    "subtitle": "Designed by me",
-    "path":"assets/files/Process/Molitoriss_SWIWS_Scene_Analysis.pdf",
-    "description": "This plot was absolutely designed by me"
+    "title": "Woneyed Herds",
+    "subtitle": "Helen Wayne Rauh Studio Theater, 2018",
+    "path":"assets/files/Process/Molitoriss_Woneyed_Herds_Lighting_Cue_List_V5.pdf",
+    "description": ""
   },
   {
-    "title": "Spring Concert",
-    "subtitle": "Designed by me",
-    "path":"assets/files/Process/Sam_Molitoriss_SWIWS_Notebook.pdf",
-    "description": "This plot was absolutely designed by me"
-  },
-  {
-    "title": "Stumpy",
-    "subtitle": "Designed by me",
-    "path":"assets/files/Process/Scene_Analysis.pdf",
-    "description": "This plot was absolutely designed by me"
+    "title": "Healed",
+    "subtitle": "Helen Wayne Rauh Studio Theater, 2018",
+    "path":"assets/files/Process/Molitoriss_Healed_Lighting_Cue_List_V5",
+    "description": ""
   }
 ];
 
