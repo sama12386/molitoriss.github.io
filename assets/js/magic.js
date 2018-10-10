94449eb74b43917657008c479e1ec6066ef319e4
Kenny Cohen <kicohen@andrew.cmu.edu>
2018-10-09 20:25:18 -0400

    Added magic sheets

---
+++ assets/js/magic.js
@@ -0,0 +1,22 @@
+var data = [
+  {
+    "title": "Magic Sheets",
+    "subtitle": "Designed by me",
+    "path":"assets/files/Plots/Magic Sheets/AIDA_Magic_Sheet.pdf",
+    "description": "This plot was absolutely designed by me"
+  },
+  {
+    "title": "Aida",
+    "subtitle": "Designed by me",
+    "path":"assets/files/Plots/Magic Sheets/MS_Magic_Sheet_V5.pdf",
+    "description": "This plot was absolutely designed by me"
+  },
+  {
+    "title": "Lunar Gala",
+    "subtitle": "Designed by me",
+    "path":"assets/files/Plots/Magic Sheets/Stumpy_MS_V2.pdf",
+    "description": "This plot was absolutely designed by me"
+  }
+];
+
+var attr = "data";
