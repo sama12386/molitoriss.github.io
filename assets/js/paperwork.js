3b12a8febe4977d2271e9dc9b8999382fa73fcbe
Kenny Cohen <kicohen@andrew.cmu.edu>
2018-10-09 20:34:07 -0400

    Added paperwork page

---
+++ assets/js/paperwork.js
@@ -0,0 +1,34 @@
+var data = [
+  {
+    "title": "LW Paperwork",
+    "subtitle": "Designed by me",
+    "path":"assets/files/Plots/Lightwright Paperwork/1-Channel Hookup.pdf",
+    "description": "This plot was absolutely designed by me"
+  },
+  {
+    "title": "Aida",
+    "subtitle": "Designed by me",
+    "path":"assets/files/Plots/Lightwright Paperwork/2-Instrument Schedule.pdf",
+    "description": "This plot was absolutely designed by me"
+  },
+  {
+    "title": "Lunar Gala",
+    "subtitle": "Designed by me",
+    "path":"assets/files/Plots/Lightwright Paperwork/3 Channel Hookup.pdf",
+    "description": "This plot was absolutely designed by me"
+  },
+  {
+    "title": "Lunar Gala",
+    "subtitle": "Designed by me",
+    "path":"assets/files/Plots/Lightwright Paperwork/LG 2018 Channel Hookup.pdf",
+    "description": "This plot was absolutely designed by me"
+  },
+  {
+    "title": "Lunar Gala",
+    "subtitle": "Designed by me",
+    "path":"assets/files/Plots/Lightwright Paperwork/3 Channel Hookup.pdf",
+    "description": "This plot was absolutely designed by me"
+  }
+];
+
+var attr = "data";
--- assets/js/programming.js
+++ assets/js/programming.js
@@ -10,24 +10,6 @@ var data = [
     "subtitle": "Designed by me",
     "path":"https://player.vimeo.com/video/293654092",
     "description": "This plot was absolutely designed by me"
-  },
-  {
-    "title": "Lunar Gala",
-    "subtitle": "Designed by me",
-    "path":"Plots/Draftings/LG_2018_Light_Plot_G.pdf",
-    "description": "This plot was absolutely designed by me"
-  },
-  {
-    "title": "Spring Concert",
-    "subtitle": "Designed by me",
-    "path":"Plots/Draftings/Spring Concert 2018 Light Plot.pdf",
-    "description": "This plot was absolutely designed by me"
-  },
-  {
-    "title": "Stumpy",
-    "subtitle": "Designed by me",
-    "path":"Plots/Draftings/Stumpy_Lighting_Plot.pdf",
-    "description": "This plot was absolutely designed by me"
   }
 ];
 
