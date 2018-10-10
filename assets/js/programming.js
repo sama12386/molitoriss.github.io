d8a6d7eead7e656c9139e6f8a41ee207a4f85f3c
Kenny Cohen <kicohen@andrew.cmu.edu>
2018-10-09 20:34:40 -0400

    Updated programming

--- assets/js/programming.js
+++ assets/js/programming.js
@@ -1,12 +1,12 @@
 var data = [
   {
-    "title": "Video 1",
+    "title": "Hog Factor",
     "subtitle": "Programmed by me",
     "path":"https://player.vimeo.com/video/293650013",
     "description": "This plot was absolutely designed by me"
   },
   {
-    "title": "Aida",
+    "title": "Instant Crush",
     "subtitle": "Designed by me",
     "path":"https://player.vimeo.com/video/293654092",
     "description": "This plot was absolutely designed by me"
