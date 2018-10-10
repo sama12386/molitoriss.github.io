21626ad95a29f11158b5e920c27e6df67b49c6f0
sama12386 <43897963+sama12386@users.noreply.github.com>
2018-10-09 22:33:34 -0400

    Update magic.js

--- assets/js/magic.js
+++ assets/js/magic.js
@@ -1,21 +1,21 @@
 var data = [
-  {
-    "title": "Magic Sheets",
-    "subtitle": "Designed by me",
-    "path":"assets/files/Plots/Magic Sheets/AIDA_Magic_Sheet.pdf",
-    "description": "This plot was absolutely designed by me"
+   {
+    "title": "Medea/Shulie",
+    "subtitle": "Helen Wayne Rauh Studio Theater, 2017",
+    "path":"assets/files/Plots/Magic Sheets/MS_Magic_Sheet_V5.pdf",
+    "description": ""
   },
   {
     "title": "Aida",
-    "subtitle": "Designed by me",
-    "path":"assets/files/Plots/Magic Sheets/MS_Magic_Sheet_V5.pdf",
-    "description": "This plot was absolutely designed by me"
+    "subtitle": "Benedum Center for Performing Arts",
+    "path":"assets/files/Plots/Magic Sheets/AIDA_Magic_Sheet.pdf",
+    "description": ""
   },
   {
-    "title": "Lunar Gala",
-    "subtitle": "Designed by me",
+    "title": "Stumpy Legs Too Short",
+    "subtitle": "Helen Wayne Rauh Theater, 2018",
     "path":"assets/files/Plots/Magic Sheets/Stumpy_MS_V2.pdf",
-    "description": "This plot was absolutely designed by me"
+    "description": "Page two allows visual indications of moving light presets to be written quickly on the magic sheet while cueing."
   }
 ];
 
