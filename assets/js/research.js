a0f5c2b460af5f1a8798f18d96792d8ad40e2c48
sama12386 <43897963+sama12386@users.noreply.github.com>
2018-10-10 00:05:46 -0400

    Update research.js

--- assets/js/research.js
+++ assets/js/research.js
@@ -1,33 +1,15 @@
 var data = [
   {
-    "title": "If We Were Birds",
-    "subtitle": "Designed by me",
-    "path":"assets/files/Process/Molitoriss_Aida_Photometric_Analysis.pdf",
-    "description": "This plot was absolutely designed by me"
+    "title": "A Streetcar Named Desire",
+    "subtitle": "",
+    "path":"assets/files/Process/research/Molitoriss_Streetcar_Visual_Research.pdf",
+    "description": ""
   },
   {
-    "title": "Aida",
-    "subtitle": "Designed by me",
-    "path":"assets/files/Process/Molitoriss_Streetcar_Binder.pdf",
-    "description": "This plot was absolutely designed by me"
-  },
-  {
-    "title": "Lunar Gala",
-    "subtitle": "Designed by me",
-    "path":"assets/files/Process/Molitoriss_SWIWS_Scene_Analysis.pdf",
-    "description": "This plot was absolutely designed by me"
-  },
-  {
-    "title": "Spring Concert",
-    "subtitle": "Designed by me",
-    "path":"assets/files/Process/Sam_Molitoriss_SWIWS_Notebook.pdf",
-    "description": "This plot was absolutely designed by me"
-  },
-  {
-    "title": "Stumpy",
-    "subtitle": "Designed by me",
-    "path":"assets/files/Process/Scene_Analysis.pdf",
-    "description": "This plot was absolutely designed by me"
+    "title": "Stumpy Legs Too Short",
+    "subtitle": "Helen Wayne Rauh Studio Theater, 2018",
+    "path":"assets/files/Process/research/Molitoriss_Stumpy_Visual_Research.pdf",
+    "description": ""
   }
 ];
 
