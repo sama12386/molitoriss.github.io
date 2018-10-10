6669cfa206ee585456faf1363b46f9d6a6b8e5d3
sama12386 <43897963+sama12386@users.noreply.github.com>
2018-10-10 00:02:13 -0400

    Update photometrics.js

--- assets/js/photometrics.js
+++ assets/js/photometrics.js
@@ -1,33 +1,9 @@
 var data = [
   {
-    "title": "If We Were Birds",
-    "subtitle": "Designed by me",
+    "title": "Aida Photometric Analysis",
+    "subtitle": "Benedum Center for Performing Arts",
     "path":"assets/files/Process/Molitoriss_Aida_Photometric_Analysis.pdf",
-    "description": "This plot was absolutely designed by me"
-  },
-  {
-    "title": "Aida",
-    "subtitle": "Designed by me",
-    "path":"assets/files/Process/Molitoriss_Streetcar_Binder.pdf",
-    "description": "This plot was absolutely designed by me"
-  },
-  {
-    "title": "Lunar Gala",
-    "subtitle": "Designed by me",
-    "path":"assets/files/Process/Molitoriss_SWIWS_Scene_Analysis.pdf",
-    "description": "This plot was absolutely designed by me"
-  },
-  {
-    "title": "Spring Concert",
-    "subtitle": "Designed by me",
-    "path":"assets/files/Process/Sam_Molitoriss_SWIWS_Notebook.pdf",
-    "description": "This plot was absolutely designed by me"
-  },
-  {
-    "title": "Stumpy",
-    "subtitle": "Designed by me",
-    "path":"assets/files/Process/Scene_Analysis.pdf",
-    "description": "This plot was absolutely designed by me"
+    "description": "Generation of beam-angle sections for a multi-set show to determine where electrics should be located and what lensing should be used."
   }
 ];
 
