f24c06788870d1ec964be1958aa79cd5b8a88c7b
sama12386 <43897963+sama12386@users.noreply.github.com>
2018-10-10 00:08:18 -0400

    Update scene.js

--- assets/js/scene.js
+++ assets/js/scene.js
@@ -1,33 +1,15 @@
 var data = [
   {
-    "title": "If We Were Birds",
-    "subtitle": "Designed by me",
-    "path":"assets/files/Process/Molitoriss_Aida_Photometric_Analysis.pdf",
-    "description": "This plot was absolutely designed by me"
+    "title": "A Streetcar Named Desire",
+    "subtitle": "Lighting Scene Analysis",
+    "path":"assets/files/Process/scene/Molitoriss_Streetcar_Scene_Analysis.pdf",
+    "description": ""
   },
   {
-    "title": "Aida",
-    "subtitle": "Designed by me",
-    "path":"assets/files/Process/Molitoriss_Streetcar_Binder.pdf",
-    "description": "This plot was absolutely designed by me"
-  },
-  {
-    "title": "Lunar Gala",
-    "subtitle": "Designed by me",
-    "path":"assets/files/Process/Molitoriss_SWIWS_Scene_Analysis.pdf",
-    "description": "This plot was absolutely designed by me"
-  },
-  {
-    "title": "Spring Concert",
-    "subtitle": "Designed by me",
-    "path":"assets/files/Process/Sam_Molitoriss_SWIWS_Notebook.pdf",
-    "description": "This plot was absolutely designed by me"
-  },
-  {
-    "title": "Stumpy",
-    "subtitle": "Designed by me",
-    "path":"assets/files/Process/Scene_Analysis.pdf",
-    "description": "This plot was absolutely designed by me"
+    "title": "See What I Wanna See",
+    "subtitle": "Lighting Scene Analysis",
+    "path":"assets/files/Process/scene/Molitoriss_SWIWS_Scene_Analysis.pdf",
+    "description": ""
   }
 ];
 
